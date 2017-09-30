d2bdc91d174f233c3b07a17aba7186f11865bb60
Waqas Khan <wuikhan@gmail.com>
2017-09-30 14:51:06 -0400

    Update linkGrabber.py

--- linkGrabber.py
+++ linkGrabber.py
@@ -1,6 +1,6 @@
 import re
 from linkGrabber import Links
 
-links = linkGrabber.Links('http://google.com')
-gb = links.find(limit=4,duplicates=False,pretty=True)
+links = Links('http://google.com')
+gb = links.find(limit=4, duplicates=False, pretty=True)
 print(gb)
